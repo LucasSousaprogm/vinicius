7761042300b44b6719ab158757aa75a974482a74
LucasSousaprogm <156460726+LucasSousaprogm@users.noreply.github.com>
2025-09-15 12:33:41 -0300

    Update poo.py

--- poo.py
+++ poo.py
@@ -1,72 +1,47 @@
 import mysql.connector
 
 conexao = mysql.connector.connect(
-    host = "localhost",
-    user = "root",
-    password = "root",
-    database = "sistema"
+
+host = "Localhost",
+username = "root",
+password = "root",
+database ="sistema"
 )
 
 cursor = conexao.cursor()
 
 
-class Usuario:
-    def __init__(self, nome, email, senha):
-        self.nome = nome
+class User:
+    def __init__(self,name: str,email : str,password: str) -> None:
+        self.name = name
         self.email = email
-        self.senha = senha
-        
+        self.password = password
 
-    def cadastrar(self, cursor, conexao):
-        cursor.execute("SELECT * FROM usuarios WHERE email = %s", (self.email,))
+    def Register(self) ->None:
+        query = ("INSERT INTO user (name,password,email) values (%s,%s,%s)")
+        data = (self.name,self.password,self.email)
+        cursor.execute('SELECT * from  user where email = %s',(self.email,))
         if cursor.fetchone():
-            print("Já existe um usuário com este e-mail.")
+            print('Ja existe esse email')
             return
-
-        cursor.execute(
-        "INSERT INTO usuarios (nome, email, senha) values (%s, %s, %s)",
-        ({self.nome}, {self.email}, {self.senha})
+        cursor.execute( 
+            query,data
         )
-    
         conexao.commit()
-        print(f"{self.nome} cadastrado com sucesso. ")
-
-    def login(self, cursor):
+        print("User cadastrado com Sucesso!")
+    
+    def Login(self) -> bool:
         cursor.execute(
-            "SELECT nome FROM usuarios WHERE email = %s AND senha = %s",
-            (self.email, self.senha)
+            "SELECT name FROM user WHERE email = %s AND password = %s",
+            (self.email, self.password)
         )
-
-        resultado = cursor.fetchone()
-        if resultado:
-            nome_real = resultado[0]
-            print(f"Bem-vindo(a), {nome_real}")
+        result = cursor.fetchone()
+        if result:
+            login_name = cursor.fetchone()
+            print(f"Welcome, {login_name}")
             return True
-        else:
-            print("E-mail ou senha incorreta")
-            return False
         
-
-while True: 
-    print("/n === Menu ===")
-    print("1 - Cadastrar usuario")
-    print("2 - Fazer lgin")
-    print("3 - Sair")
-    opcao = input ("Escolha uma opcao: ")
-
-    if opcao == "1":
-        nome = input("Nome: ")
-        email = input("E-mail: ")
-        senha = input("Senha: ")
-        usuario = input("Usuario: ")
-        usuario.cadastrar(cursor, conexao)
-    elif opcao == "2":
-        email = input("Senha: ")
-        senha = input("Senha: ")
-        usuario = usuario("",email,senha)
-        usuario.login(cursor)
-    elif opcao == "3":
-        cursor.close
-    else:
-        print("Insira uma opcao valida")
+        if not cursor.fetchone():
+            print("Email or Password incorrect")
+            return False
 
