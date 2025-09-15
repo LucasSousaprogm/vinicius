0463ed1c1c8c41c8e7b98bc5bb4a9f21bb6a6e17
LucasSousaprogm <156460726+LucasSousaprogm@users.noreply.github.com>
2025-09-15 12:34:31 -0300

    Update poo.py

--- poo.py
+++ poo.py
@@ -45,3 +45,34 @@ class User:
             print("Email or Password incorrect")
             return False
 
+
+
+ while True:
+        print('MENU')
+        print('1 - Register ')
+        print('2 - Login ')
+        print('3 - Exit ')
+        try:
+            choice_user = int(input('Choose an option: '))
+            if choice_user == 1:
+                print('\n--- Register ---')
+                name = input('Name: ')
+                email = input('Email: ')
+                password = input('Password: ')
+                user = User(name, email, password)
+                user.Register()
+            
+            if choice_user == 2:
+                print('\n--- Login ---')
+                email = input('Email: ')
+                password = input('Password: ')
+                user = User(name="", email=email, password=password) 
+                user.Login()
+            if choice_user == 3:
+                print('Exiting the system. Goodbye!')
+                break
+                
+        except ValueError:
+            print('Invalid Input.')
+
+
